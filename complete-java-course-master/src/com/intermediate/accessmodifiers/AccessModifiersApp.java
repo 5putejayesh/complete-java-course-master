3e6dd9751d0a755e5aa24e418a4e26dbbeba68d5
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-08-26 17:48:57 +0530

    access modifiers

---
+++ complete-java-course-master/src/com/intermediate/accessmodifiers/AccessModifiersApp.java
@@ -0,0 +1,16 @@
+package com.intermediate.accessmodifiers;
+/*
+ 	Access Modifiers:-
+ 	    They are used to set accessibility of classes,methods,class variables and
+ 	    other Java elements.
+ 	
+ 	Default :-   only within package.
+ 	Private :-   witjin the class only.
+ 	Protected :- within tha package and all subclasses.
+ 	Public    :- visible everywhere.
+ 	
+ 	Access variables can't be applied to local variables.
+ */
+public class AccessModifiersApp {
+
+}
