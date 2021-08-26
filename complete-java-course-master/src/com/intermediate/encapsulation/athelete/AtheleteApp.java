91e03cef71dbb83074074f3be902ebad44324c0e
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-08-26 22:14:02 +0530

    encapsulation

---
+++ complete-java-course-master/src/com/intermediate/encapsulation/athelete/AtheleteApp.java
@@ -0,0 +1,15 @@
+package com.intermediate.encapsulation.athelete;
+/*
+ * Encapsulation:-
+ * 		Wrapping up data into single unit (a class) and it prevents data
+ * 		being accessed by code outside.
+ * 
+ * 		Data hiding.
+ */
+public class AtheleteApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
