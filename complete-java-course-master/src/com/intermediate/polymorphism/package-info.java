902316a2d5ce37add378655ee7c58533840906ff
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-08-28 23:00:06 +0530

    Polymorphism.

---
+++ complete-java-course-master/src/com/intermediate/polymorphism/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+/**
+ * @author PachputeJayesh
+ *Polumorphism:-
+ *	Polymorphism in java, is an object's ability to take on various forms.
+ *  It enables us to complete one action in many different ways
+ *	Allows you to have many forms with same purpose.
+ *
+ *  Types of Polimorphism :-
+ * 	 	1.Compile Time Polimorphism (method overloading)
+ *  	2.Run Time Polymorphism (method overriding)
+ *  
+ *Method Overriding:-
+ *	Process of implementing parent class method in subclass is called method overriding.
+ *	Creating new body for the inherited functions.
+ *	Changing implementation as per requirement.
+ *	Inheritance is must.
+ *	Method signature must be same.
+ *
+ *Method Overloading:-
+ *	is a feature that  enables class have multiple methods with the same name.
+ *	Creating a new 
+ *
+ *	Private methods can't be overridden :-
+ *			because when superclass method is private it will not be visible to subclass.
+ * 			Whatever the methods you write in subclass will be treated as new method and
+ * 			not an overridden method.
+ *	
+ *
+ */
+package com.intermediate.polymorphism;
