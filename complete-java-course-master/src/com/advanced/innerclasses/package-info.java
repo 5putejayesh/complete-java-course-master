898a631d9dc6c80b1e2eb7097ff6baeb8c37217c
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-09-01 21:59:06 +0530

    inner classes.

---
+++ complete-java-course-master/src/com/advanced/innerclasses/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * @author PachputeJayesh
+ *Anonymous Class:-
+ *	Local defination of a class.
+ *	Has no name.
+ *	Can be instantiated only once.
+ *	Usually declared inside a method or code block ,the curly braces ending with semicolon.
+ *	Is accessible only at the point where it is defined.
+ *	Doesn't have a constructor simply because it doesn't have name.
+ *	Can't be static.
+ *	usually extend subclasses or implement interfaces.
+ *
+ *Usage Of Inner Classes:-
+ *	Good option if you need a class that will be used only in the outer class.
+ *	Static nested classes are the most useful.
+ *	Can increase encapsulation-TRUE but makes your code less readable.
+ *	
+ *	
+ */
+package com.advanced.innerclasses;
