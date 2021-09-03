3978680dafae60f2caf098d7496db65fe8d38140
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-09-03 23:14:00 +0530

    Linked List added.

---
+++ complete-java-course-master/src/com/advanced/collections/linkedlist/LinkedListApp.java
@@ -0,0 +1,50 @@
+package com.advanced.collections.linkedlist;
+/*
+ * Features Of LinkedList:-
+ * 		Dynamic memory allocation.
+ * 		No initial capacity is required.
+ * 		Insert and delete operations are not performance wise expensive.
+ * 
+ * 		Retrieving element from linked list is slower that array list.
+ * 
+ * 		Memory consumption is higher that array list as linked list stores element and pointer 
+ * 		to it's neighbor.
+ * 
+ */
+import java.util.LinkedList;
+
+public class LinkedListApp {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> morningRoutine = new LinkedList<>();
+
+        if (!morningRoutine.isEmpty()) {
+            System.out.println("The first element of the list: " + morningRoutine.getFirst());
+        }
+
+        if (!morningRoutine.isEmpty() && morningRoutine.peekFirst() != null) {
+            System.out.println("The first element of the list: " + morningRoutine.peekFirst().toUpperCase());
+        }
+
+//        morningRoutine.add("Brushing my teeth");
+//        morningRoutine.add("Taking the dog for a walk");
+//        morningRoutine.add("Doing 32 push-ups");
+
+        morningRoutine.add("Brushing my teeth");
+        morningRoutine.addFirst("Taking the dog for a walk");
+        morningRoutine.push("Doing 32 push-ups");
+        morningRoutine.offer("Reading 1 chapter of a book");
+
+//        System.out.println("I used pop function: " + morningRoutine.pop());
+
+        System.out.println(morningRoutine);
+
+        System.out.println("I completed the first element on my list: " + morningRoutine.pollFirst());
+        System.out.println("I completed the last element on my list: " + morningRoutine.pollLast());
+
+        System.out.println(morningRoutine);
+
+    }
+}
+
