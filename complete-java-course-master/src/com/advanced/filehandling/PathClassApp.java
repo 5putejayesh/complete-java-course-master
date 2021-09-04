916f6d768b3cc91cfe5123251e9af9f31846a1dc
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-09-04 23:55:28 +0530

    Path class.

---
+++ complete-java-course-master/src/com/advanced/filehandling/PathClassApp.java
@@ -0,0 +1,23 @@
+package com.advanced.filehandling;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathClassApp {
+
+	public static void main(String[] args) {
+		
+		String FILE_PATH="src/com/advanced/filehandling/PrintWriter.txt";
+		
+		Path path=Paths.get(FILE_PATH);
+		
+		System.out.println("toString: "+path.toString());
+		System.out.println("Get File Name: "+path.getFileName());
+		System.out.println("Get Name(0) "+path.getName(0));
+		System.out.println("Get NameCount: "+path.getNameCount());
+		System.out.println("SubPath(0,2): "+path.subpath(0, 2));
+		System.out.println("getParent: "+path.getParent());
+		System.out.println("getRoot: "+path.getRoot());
+		
+	}
+}
